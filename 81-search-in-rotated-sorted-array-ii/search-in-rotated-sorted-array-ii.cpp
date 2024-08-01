ae6ded1c2ea17b0a5c04172f7b1ae244af84eca2
Subhodip Ghosh <macy16661@gmail.com>
2024-08-01 14:27:02 +0530

    Time: 10 ms (12.28%) | Memory: 16.9 MB (11.18%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-    int low = 0;
-    int high = nums.size() - 1;
+        int low = 0;
+        int high = nums.size()-1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
+        while(low<=high) {
+            int mid = (low+high) /2;
 
-        if (nums[mid] == target) return true;
+            if(nums[mid] == target) return true;
 
-        // If duplicates are found, adjust low and high
-        if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
-            low++;
-            high--;
-        } else if (nums[low] <= nums[mid]) {
-            // If left side is sorted
-            if (nums[low] <= target && target < nums[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                high--;
+                low++;
+                continue;
             }
-        } else {
-            // If right side is sorted
-            if (nums[mid] < target && target <= nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+
+            // If left is sorted
+            if(nums[low] <= nums[mid]) {
+                // if target lies b/w low and mid
+                if(nums[low] <= target && target <= nums[mid]){
+                    high = mid-1;
+                }else{
+                    low =mid+1;
+                }
+            }else{ //if right is sorted
+                // if target lise b/w mid and high
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
         }
+        return false;
     }
-    return false;
-}
 };
