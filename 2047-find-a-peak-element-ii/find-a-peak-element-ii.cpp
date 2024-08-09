c896f930892eaeb0e3b9d3f0f0f223242e3b61da
Subhodip Ghosh <macy16661@gmail.com>
2024-08-09 18:59:59 +0530

    Time: 110 ms (95.58%) | Memory: 49.3 MB (20.61%) - LeetSync

---
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findMaxIndex(vector<vector<int>>& mat, int n, int m, int col){
+        int index=-1, value = -1;
+
+        for(int i=0; i<n; i++){
+            if(mat[i][col] > value){
+                value = mat[i][col];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+
+        int low=0, high=m-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int maxRowIndex = findMaxIndex(mat, n,m,mid);
+
+            int left = mid-1 >= 0 ? mat[maxRowIndex][mid - 1]: -1;
+            int right = mid+1 < m ? mat[maxRowIndex][mid + 1]: -1;
+
+            if(mat[maxRowIndex][mid] > left && mat[maxRowIndex][mid] > right){
+                return {maxRowIndex, mid};
+            }else if(mat[maxRowIndex][mid] < left) {
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return {-1,-1};
+    }
+};
