37af4b65e54e67cc2cb7564335e9b6ba67bcf0d8
Subhodip Ghosh <macy16661@gmail.com>
2025-05-19 12:04:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (12.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,48 +1,36 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    int m = matrix[0].size();
-    int col0 = 1;
-    // step 1: Traverse the matrix and
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                // mark i-th row:
-                matrix[i][0] = 0;
-
-                // mark j-th column:
-                if (j != 0)
-                    matrix[0][j] = 0;
-                else
-                    col0 = 0;
-            }
+    void setRowCol(int i, int j, vector<vector<int>>& newMat) {
+        // Setting row zero
+        for(int k=0; k<newMat[0].size(); k++) {
+            newMat[i][k] = 0;
         }
+
+        // Setting col zero
+        for(int k=0; k<newMat.size(); k++) {
+            newMat[k][j] = 0;
+        }
+
+
     }
 
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                // check for col & row:
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+    void setZeroes(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        int M = matrix[0].size();
+        // initialize it with zero
+        // vector<vector<int>> newMat(N, (matrix, 0));
+
+        //Deep copy
+        vector<vector<int>> newMat = matrix;
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(matrix[i][j] == 0) {
+                    setRowCol(i, j, newMat);
                 }
             }
         }
-    }
 
-    //step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
-        }
-    }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
-    }
+        matrix = newMat;
     }
 };
