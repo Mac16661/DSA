0e4d8a4cd0695b305128981d2ae505104d0d20af
Subhodip Ghosh <macy16661@gmail.com>
2025-05-22 13:28:49 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (30.59%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
+    // time nlogn
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0, cnt2 = 0;
-        int ele1=-1, ele2=-1;
+        int n = nums.size();
 
-        for(int i=0; i<nums.size(); i++) {
-            // For element 1
-            if(cnt1 == 0 && nums[i] != ele2){
-                cnt1 = 1;
-                ele1 = nums[i];
-            }else if(nums[i] == ele1) cnt1++;
-            // For element 2
-            else if(cnt2 == 0 && nums[i] != ele1) {
-                cnt2 = 1;
-                ele2 = nums[i];
-            }else if(nums[i] == ele2) cnt2++;
+        if(n <= 1) return nums;
+
+        sort(nums.begin(), nums.end());
+        int cnt = 0;
+        
+        vector<int> ans;
+
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i-1] == nums[i]) cnt++;
             else{
-                cnt1--;cnt2--;
+                if((cnt+1) > n/3) {
+                    ans.push_back(nums[i-1]);
+                }
+                cnt = 0;
             }
         }
 
-         vector<int> result;
-        int threshold = nums.size() / 3; // Threshold for majority element
-
-        cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (ele1 == nums[i]) {
-                cnt1++;
-            } else if (ele2 == nums[i]) {
-                cnt2++;
-            }
+        // for the last element
+        if((cnt+1) > n/3) {
+            ans.push_back(nums[n-1]);
         }
 
-        if(cnt1 > threshold) result.push_back(ele1);
-        if(cnt2 > threshold) result.push_back(ele2);
-
-        return result;
+        return ans;
     }
 };
