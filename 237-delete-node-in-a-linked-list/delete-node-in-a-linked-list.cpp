296b2822f892ad94c179d0224b0fc9223d5dc5b0
Subhodip Ghosh <macy16661@gmail.com>
2025-06-20 01:07:46 +0530

    Time: 10 ms (33.78%) | Memory: 12.5 MB (18.21%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -8,20 +8,12 @@
  */
 class Solution {
 public:
+    
     void deleteNode(ListNode* node) {
-        ListNode* nextNode = node->next;
-        ListNode* prev;
-        // cout<<nextNode->val;
-        while(nextNode != NULL){
-            node->val = nextNode->val;
-            nextNode = nextNode->next;
-            prev = node;
-            node = node->next;
-        }
-        // delete node;
-        // cout<<prev->val;
-        prev->next = NULL;
-        delete node;
-        // cout<<prev->next->val;
+       if(node==nullptr) return;
+
+        // Just override the next node of the given node 
+       node->val = node->next->val;
+       node->next = node->next->next;
     }
 };
