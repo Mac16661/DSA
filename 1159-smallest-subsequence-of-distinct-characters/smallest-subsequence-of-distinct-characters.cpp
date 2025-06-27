01777335a9d7612b00516f16d04203c077d02727
Subhodip Ghosh <macy16661@gmail.com>
2025-06-28 01:54:53 +0530

    Time: 3 ms (12.66%) | Memory: 9.2 MB (9.06%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        unordered_map<char,int> lastOcc;
+        for(int i=0; i<s.size(); i++) {
+            lastOcc[s[i]]=i;
+        }
+
+        stack<char> st;
+        unordered_set<char> visited;
+
+        for(int i=0; i<s.size(); i++) {
+            if(visited.find(s[i]) != visited.end()) continue;
+
+            while(!st.empty() && s[i] < st.top() && i < lastOcc[st.top()]) {
+                visited.erase(st.top());
+                st.pop();
+            }
+
+            visited.insert(s[i]);
+            st.push(s[i]);
+        }
+
+        string ans;
+
+        while(!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
+    }
+};
