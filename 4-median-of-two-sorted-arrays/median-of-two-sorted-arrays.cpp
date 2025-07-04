07eb3d1f17c8aa99849ec8942750a3ee8eb75094
Subhodip Ghosh <macy16661@gmail.com>
2025-07-04 20:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (59.80%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,49 +1,43 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        
-        // Ensure nums1 is the smaller array for simplicity
-        if (n1 > n2)
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int n = n1 + n2;
-        int left = (n1 + n2 + 1) / 2; // Calculate the left partition size
-        int low = 0, high = n1;
-        
-        while (low <= high) {
-            int mid1 = (low + high) /2; // Calculate mid index for nums1
-            int mid2 = left - mid1; // Calculate mid index for nums2
-            
-            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            
-            // Determine values of l1, l2, r1, and r2
-            if (mid1 < n1)
-                r1 = nums1[mid1];
-            if (mid2 < n2)
-                r2 = nums2[mid2];
-            if (mid1 - 1 >= 0)
-                l1 = nums1[mid1 - 1];
-            if (mid2 - 1 >= 0)
-                l2 = nums2[mid2 - 1];
-            
-            if (l1 <= r2 && l2 <= r1) {
-                // The partition is correct, we found the median
-                if (n % 2 == 1)
-                    return max(l1, l2);
-                else
-                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
-            }
-            else if (l1 > r2) {
-                // Move towards the left side of nums1
-                high = mid1 - 1;
-            }
-            else {
-                // Move towards the right side of nums1
-                low = mid1 + 1;
-            }
-        }
-        
-        return 0;
+    int findKth(int k, vector<int>& nums1, vector<int>& nums2) {
+    int i = 0, j = 0, cnt = 0;
+    int n1 = nums1.size(), n2 = nums2.size();
+
+    while (i < n1 && j < n2) {
+        int val;
+        if (nums1[i] <= nums2[j])
+            val = nums1[i++];
+        else
+            val = nums2[j++];
+
+        if (cnt == k)
+            return val;
+        cnt++;
     }
+
+    while (i < n1) {
+        if (cnt == k)
+            return nums1[i];
+        cnt++, i++;
+    }
+
+    while (j < n2) {
+        if (cnt == k)
+            return nums2[j];
+        cnt++, j++;
+    }
+
+    return -1; // unreachable
+}
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    int total = nums1.size() + nums2.size();
+    if (total % 2 == 0) {
+        return (findKth(total / 2, nums1, nums2) + findKth(total / 2 - 1, nums1, nums2)) / 2.0;
+    } else {
+        return findKth(total / 2, nums1, nums2);
+    }
+}
+
 };
