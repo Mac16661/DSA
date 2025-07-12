667c6736e45ee3efd25823830db4ea6846cd5a21
Subhodip Ghosh <macy16661@gmail.com>
2025-07-12 08:39:32 +0530

    Time: 8 ms (16.53%) | Memory: 23.7 MB (11.20%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,41 +1,54 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string ans;
-        int left=0;
-        int right = 0;
+        // string ans;
+        // int left=0;
+        // int right = 0;
 
-        // trim down all the whitespace from start;
-         while (right < s.size() && s[right] == ' ') right++;
-        s = s.substr(right);
+        // // trim down all the whitespace from start;
+        //  while (right < s.size() && s[right] == ' ') right++;
+        // s = s.substr(right);
 
-        // Reset pointers
-        left = 0;
-        right = 0;
+        // // Reset pointers
+        // left = 0;
+        // right = 0;
 
-        while (right < s.size()) {
-            if (s[right] == ' ') {
-                // Extract the word (length = right - left)
-                string word = s.substr(left, right - left);
-                if (!word.empty()) {
-                    ans = word + " " + ans;
-                }
-                // Skip whitespaces
-                while (right < s.size() && s[right] == ' ') right++;
-                left = right;
+        // while (right < s.size()) {
+        //     if (s[right] == ' ') {
+        //         // Extract the word (length = right - left)
+        //         string word = s.substr(left, right - left);
+        //         if (!word.empty()) {
+        //             ans = word + " " + ans;
+        //         }
+        //         // Skip whitespaces
+        //         while (right < s.size() && s[right] == ' ') right++;
+        //         left = right;
+        //     } else {
+        //         right++;
+        //     }
+        // }
+
+        // if (left < s.size()) {
+        // string word = s.substr(left, right - left);
+        //     ans = word + " " + ans;
+        // }
+
+        // // Trim trailing space
+        // if (!ans.empty() && ans.back() == ' ') {
+        //     ans.pop_back();
+        // }
+
+        // return ans;
+
+        stringstream ss(s);
+        string word, ans;
+
+        while (ss >> word) { // this skips multiple spaces automatically
+            if (!ans.empty()) {
+                ans = word + " " + ans;
             } else {
-                right++;
+                ans = word;
             }
-        }
-
-        if (left < s.size()) {
-        string word = s.substr(left, right - left);
-            ans = word + " " + ans;
-        }
-
-        // Trim trailing space
-        if (!ans.empty() && ans.back() == ' ') {
-            ans.pop_back();
         }
 
         return ans;
