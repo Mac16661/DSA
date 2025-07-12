d3fe90812162b1145c8f8ba0e5da75e986ab1b10
Subhodip Ghosh <macy16661@gmail.com>
2025-07-12 11:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.49%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if(s.length()==0) return 0;
-
+        // trim the whitespace
         int i=0;
-         while(i<s.size() && s[i]== ' ') {
-             i++;
-         }     
-        s = s.substr(i); //i ---> last of string
-        
-        int sign = +1;
-        long ans = 0;
-        
-        if(s[0] == '-') sign = -1;
-        
-        int MAX = INT_MAX, MIN = INT_MIN;
-        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
-		
-        while(i < s.length()) {
-            if(s[0] == ' ' || !isdigit(s[i])) break;
-            
-            ans = ans * 10 + s[i]-'0';
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
-            
+
+        while(s[i] == ' ') i++;
+
+        // check if -ve no
+        int sign = s[i] == '-' ? -1 : 1;
+        if(sign == -1 or s[i] == '+') i++;
+
+        // init sum = 1/10
+        int sum = 0;
+
+        // loop through until non-digit val is found
+        while(i<s.size() && s[i] >= '0' && s[i] <='9'){
+            // sum = sum * 10 + current val;
+            int digit =  s[i] - '0';
+
+            if(sum > INT_MAX / 10 || (sum == INT_MAX / 10 && digit > 7)) {
+                return sign == 1 ? INT_MAX : INT_MIN;
+            }
+
+            sum = sum*10 + digit;
+            if(sum < INT_MIN) return INT_MIN;
+            if(sum > INT_MAX) return INT_MAX;
+            // cout<<sum<<endl;
             i++;
         }
-        
-        return (int)(sign*ans);
+            
+        return sum*sign;
+        // return sum * sign
     }
 };
