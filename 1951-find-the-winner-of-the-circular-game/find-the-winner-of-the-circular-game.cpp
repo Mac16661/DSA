d98ae373c9fdbcf0f5956789171c7369adbfd4cb
Subhodip Ghosh <macy16661@gmail.com>
2025-07-27 00:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.78%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int findTheWinner(int n, int k) {
-        int winner = 0;
+    // DP solution 
+    // int findTheWinner(int n, int k) {
+    //     int winner = 0;
         
-        // need atleast 2 number, i is representing the total no
-        for(int i=2; i<=n;i++) {
-            winner = (winner + k) % i;
-        }
+    //     // need atleast 2 number, i is representing the total no
+    //     for(int i=2; i<=n;i++) {
+    //         winner = (winner + k) % i;
+    //     }
 
-        // when i = 2 -> if there are 2 player left who would win
-        // fi  i = 3 -> if there are 3 players, who would win (doing the whole thing in bottoms up approach)
+    //     // when i = 2 -> if there are 2 player left who would win
+    //     // fi  i = 3 -> if there are 3 players, who would win (doing the whole thing in bottoms up approach)
 
-        return winner+1;
+    //     return winner+1;
+    // }
+
+    // Recursion with subproblem solution 
+    int rec(int n, int k){
+        if(n==1) return 0;
+
+        return (rec(n-1, k) + k) % n;
+    }
+
+    int findTheWinner(int n, int k) {
+        return rec(n, k) + 1;
     }
 };
