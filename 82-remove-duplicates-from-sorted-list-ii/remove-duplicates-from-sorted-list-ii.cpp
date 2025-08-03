a30f56150adab5d7ee640afd41acff3b5b36ef1d
Subhodip Ghosh <macy16661@gmail.com>
2025-08-03 21:04:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (42.61%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -11,27 +11,45 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        // Traverse through list chech if curr->next->val == curr->val: temp = curr->next; curr->next = curr->next->next; delete temp; do it until curr->next != nullpter
 
         // Brute
-        std::map<int, int> mpp;
+        // std::map<int, int> mpp;
 
-        while(head != nullptr) {
-            mpp[head->val]++;
-            head = head->next;
-        }
+        // while(head != nullptr) {
+        //     mpp[head->val]++;
+        //     head = head->next;
+        // }
 
-        ListNode* newNode = new ListNode(-1);
-        ListNode* temp = newNode;
+        // ListNode* newNode = new ListNode(-1);
+        // ListNode* temp = newNode;
 
-        for(auto mp: mpp) {
-            if(mp.second <= 1) {
-                temp->next = new ListNode(mp.first);
-                temp = temp->next;
+        // for(auto mp: mpp) {
+        //     if(mp.second <= 1) {
+        //         temp->next = new ListNode(mp.first);
+        //         temp = temp->next;
+        //     }
+        //     // cout<<mp.first<<" "<<mp.second<<endl;
+        // }
+
+        // return newNode->next;
+
+        // Optimal
+        ListNode* dummy = new ListNode(0, head); // Handles head removal
+        ListNode* prev = dummy;
+
+        while (head != NULL) {
+            // Detect duplicates
+            if (head->next != NULL && head->val == head->next->val) {
+            while (head->next != NULL && head->val == head->next->val) {
+                    head = head->next; 
+                }
+                prev->next = head->next; // Remove entire group
+            } else {
+                prev = prev->next; // Unique value, move prev forward
             }
-            // cout<<mp.first<<" "<<mp.second<<endl;
+            head = head->next; // Move head forward in all cases
         }
 
-        return newNode->next;
+        return dummy->next;
     }
 };
