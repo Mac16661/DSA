d837157bd63b6a00b3ed71db6e3d5db645eba95a
Subhodip Ghosh <macy16661@gmail.com>
2025-08-06 20:55:29 +0530

    Time: 19 ms (11.87%) | Memory: 22.9 MB (42.09%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    long long totalTime(vector<int> &v, int hourly) {
+    long long time_to_eat(vector<int> &v, int hourly) {
         long long totalH = 0;
         int n = v.size();
         //find total hours:
@@ -21,15 +21,24 @@ public:
         int high = maxi;
         int ans = high;
 
-        while(low<=high) {
-            int mid = (low+high)/2;
-            long long totalH = totalTime(piles, mid);
+        // for(int i=1; i<=max_val; i++) {
+        //     int time_taken = time_to_eat(piles, i);
+        //     if(time_taken <= h) return i;
+        // }
 
-            if(totalH <= h){
-                high = mid-1;
+        // I guess i am trying to find the lower bound
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            long long time_taken = time_to_eat(piles, mid);
+
+            // cout<<low<<" "<<mid<<" "<<high<<" - > "<<time_taken<<endl;
+
+            if(time_taken <= h) {
                 ans = mid;
+                high = mid - 1;
             }else{
-                low = mid+1;
+                low = mid + 1;
             }
         }
 
