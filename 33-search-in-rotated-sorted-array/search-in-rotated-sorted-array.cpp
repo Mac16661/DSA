7425852a19c04372f98da99ac1dc8802c2d5ae4a
Subhodip Ghosh <macy16661@gmail.com>
2025-08-06 13:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int low = 0;
-        int high = nums.size()-1;
+        int high = nums.size() - 1;
 
-        while(low<=high){
-            int mid = (low+high) /2;
+        while(low <= high) {
+            int mid = (low+high)/2;
 
-            if(nums[mid] == target) return mid;
+            if(nums[mid] == target) return  mid;
 
-            // If left side is sorted
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target<= nums[mid] ){
-                    high = mid -1;
+            // # Move here if only left part is sorted
+            if(nums[low] <= nums[mid]) {
+                if(nums[low] <= target && nums[mid] >= target) { // check if it is lies in between
+                    high = mid - 1;
                 }else{
-                    low = mid+1;
+                    low = mid + 1;
                 }
-
-            }else{ // If right side is sorted
-
-                if(nums[mid] <= target && target <= nums[high] ){
+            // Move here only if right part is sorted
+            }else {
+                if(nums[mid] <= target && nums[high] >= target) { // check if taret lies in between
                     low = mid+1;
-                }else{
-                    high = mid -1;
+                }else {
+                    high = mid-1;
                 }
             }
         }
