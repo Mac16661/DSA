650410db76d332b7321199f999a419a067c0933d
Subhodip Ghosh <macy16661@gmail.com>
2025-08-07 11:49:03 +0530

    Time: 20 ms (77.50%) | Memory: 23.6 MB (32.58%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -21,18 +21,31 @@ public:
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> pathp;
-        vector<TreeNode*> pathq;
+        // vector<TreeNode*> pathp;
+        // vector<TreeNode*> pathq;
 
-        BSTPath(root, p, pathp);
-        BSTPath(root, q, pathq);
+        // BSTPath(root, p, pathp);
+        // BSTPath(root, q, pathq);
 
-        for(int i=pathp.size()-1; i>=0; i--) {
-            for(int j=pathq.size()-1; j>=0; j--) {
-                if(pathp[i]->val == pathq[j]->val){
-                    return pathp[i];
-                }
-            }
+        // for(int i=pathp.size()-1; i>=0; i--) {
+        //     for(int j=pathq.size()-1; j>=0; j--) {
+        //         if(pathp[i]->val == pathq[j]->val){
+        //             return pathp[i];
+        //         }
+        //     }
+        // }
+
+        // Optimal code
+
+        if(root == nullptr) return root;
+        int curr = root->val;
+
+        if(curr < p->val && curr < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }else if(curr > p->val && curr > q->val) {
+            return lowestCommonAncestor(root->left, p , q);
+        }else {
+            return root;
         }
 
         return nullptr;
