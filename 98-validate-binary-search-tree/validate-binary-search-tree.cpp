13efa61263e491de8820d8332a186912255aa07a
Subhodip Ghosh <macy16661@gmail.com>
2025-08-08 11:57:51 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (17.73%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -33,34 +33,49 @@ public:
         return true;
     }
 
+    bool valid(TreeNode* node, long left, long right) {
+        if (!node) {
+            return true;
+        }
+        if (!(left < node->val && node->val < right)) {
+            return false;
+        }
+        return valid(node->left, left, node->val) && valid(node->right, node->val, right);
+    }
+
     bool isValidBST(TreeNode* root) {
         if(root == nullptr) return true;
 
-        TreeNode* temp = root;
+        // Brute force 0(n)^2
+        // TreeNode* temp = root;
 
-        // Performing dfs
-        stack<TreeNode*> st;
-        st.push(root);
+        // // Performing dfs
+        // stack<TreeNode*> st;
+        // st.push(root);
 
-        while(!st.empty()) {
-            int size = st.size();
+        // while(!st.empty()) {
+        //     int size = st.size();
 
-            for(int i=0; i<size; i++) {
-                TreeNode* f = st.top();
-                st.pop();
+        //     for(int i=0; i<size; i++) {
+        //         TreeNode* f = st.top();
+        //         st.pop();
 
-                if(f->right) {
-                    st.push(f->right);
-                    if(!checkRightSubtree(f->right, f)) return false;
-                }
+        //         if(f->right) {
+        //             st.push(f->right);
+        //             if(!checkRightSubtree(f->right, f)) return false;
+        //         }
 
-                if(f->left) {
-                    st.push(f->left);
-                    if(!checkLeftSubtree(f->left, f)) return false;
-                }
-            }
-        }
+        //         if(f->left) {
+        //             st.push(f->left);
+        //             if(!checkLeftSubtree(f->left, f)) return false;
+        //         }
+        //     }
+        // }
 
-        return true;
+        // return true;
+
+        // Optimal
+
+        return valid(root, LONG_MIN, LONG_MAX); // root, lower bound, upper bound
     }
 };
