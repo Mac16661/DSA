54f01e776f370886b9a00ec37025627590f6b3ea
Subhodip Ghosh <macy16661@gmail.com>
2025-08-08 11:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (75.24%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkLeftSubtree(TreeNode* next, TreeNode* root) {
+        if(next==nullptr) return true;
+
+        if(next->val >= root->val) return false;
+
+        if(!checkLeftSubtree(next->left, root)) return false;
+        if(!checkLeftSubtree(next->right, root)) return false;
+
+        return true;
+    }
+
+    bool checkRightSubtree(TreeNode* next, TreeNode* root) {
+        if(next == nullptr) return true;
+
+        if(next->val <= root->val) return false;
+
+        if(!checkRightSubtree(next->left, root)) return false;
+        if(!checkRightSubtree(next->right, root)) return false;
+        
+        return true;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        if(root == nullptr) return true;
+
+        TreeNode* temp = root;
+
+        // Performing dfs
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()) {
+            int size = st.size();
+
+            for(int i=0; i<size; i++) {
+                TreeNode* f = st.top();
+                st.pop();
+
+                if(f->right) {
+                    st.push(f->right);
+                    if(!checkRightSubtree(f->right, f)) return false;
+                }
+
+                if(f->left) {
+                    st.push(f->left);
+                    if(!checkLeftSubtree(f->left, f)) return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
