a2d624bd7f1f9f3bac093eb7be037824da481470
Subhodip Ghosh <macy16661@gmail.com>
2025-08-11 23:28:50 +0530

    Time: 17 ms (17.88%) | Memory: 35.2 MB (10.87%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    int findDays(vector<int> &weights, int cap) {
-    int days = 1; //First day.
-    int load = 0;
-    int n = weights.size(); //size of array.
-    for (int i = 0; i < n; i++) {
-        if (load + weights[i] > cap) {
-            days += 1; //move to next day
-            load = weights[i]; //load the weight.
+    int dayReq(vector<int>& weights, int capacity) {
+         int cnt = 1;  // at least one day needed
+        int sum = 0;
+        for (int w : weights) {
+            if (sum + w > capacity) { // exceed capacity → start new day
+                cnt++;
+                sum = 0;
+            }
+            sum += w;
         }
-        else {
-            //load the weight on the same day.
-            load += weights[i];
-        }
+        return cnt;
     }
-    return days;
-}
 
-int shipWithinDays(vector<int> &weights, int d) {
-    //Find the maximum and the summation:
-    int low = *max_element(weights.begin(), weights.end());
-    int high = accumulate(weights.begin(), weights.end(), 0);
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        int numberOfDays = findDays(weights, mid);
-        if (numberOfDays <= d) {
-            //eliminate right half
-            high = mid - 1;
+    int shipWithinDays(vector<int>& weights, int days) {
+         int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int numberOfDays = dayReq(weights, mid);
+            if (numberOfDays <= days) {
+                //eliminate right half
+                high = mid - 1;
+            }
+            else {
+                //eliminate left half
+                low = mid + 1;
+            }
         }
-        else {
-            //eliminate left half
-            low = mid + 1;
-        }
+        return low;
     }
-    return low;
-}
-
 };
