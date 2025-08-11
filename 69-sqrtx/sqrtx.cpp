016fdca82f51dab1d36794ab849a9a6c75ed6134
Subhodip Ghosh <macy16661@gmail.com>
2025-08-11 20:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.62%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,10 +3,20 @@ public:
     int mySqrt(int x) {
         if(x<=1) return x;
 
-        long long i=1;
+        long low = 1, high = x;
+        long long ans = 1;
 
-        while(i*i <= x) i++;
+        while(low <= high) {
+            long long mid = (low+high)/2;
 
-        return (int)i-1; 
+            if(mid*mid <= x*1ll) {
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+
+        return ans;
     }
 };
