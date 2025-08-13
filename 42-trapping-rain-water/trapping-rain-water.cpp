6258ad5fe4d64257778afefad6db0945f5b55987
Subhodip Ghosh <macy16661@gmail.com>
2025-08-13 21:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (19.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
-    // CAN BE SOLVED BY PRFIX/SUFFIX SUM OR MONTONIC STACK
-    // int trap(vector<int>& height) {
-    //     int n = height.size() - 1;
-    //     int ans = 0;
-
-    //     // not counting 0th and nth element
-    //     for(int i=1; i<n; i++) {
-    //         // for current node we need to calculate maxHeight->leftside & maxHeight->rightside.
-    //         int rightMax = 0;
-    //         for(int r=i; r<n+1; r++){
-    //             rightMax = max(rightMax, height[r]);
-    //         }
-
-    //         int leftMax = 0;
-    //         for(int l=i; l>=0; l--){
-    //             leftMax = max(leftMax, height[l]);
-    //         }
-
-    //         int minWater = min(rightMax, leftMax);
-    //         ans += minWater-height[i];
-    //     }
-
-    //     return ans;
-        
-    // }
-
-    // TODO: OPTIMAL -> Prefix sum
+    // This problem is about total how much u can store
     int trap(vector<int>& height) {
         int n = height.size();
-    if (n == 0) return 0;
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
 
-    vector<int> leftH(n), rightH(n);
-    int ans = 0;
+        left[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = max(left[i - 1], height[i]);
+        }
 
-    // Build left max array
-    leftH[0] = height[0];
-    for (int i = 1; i < n; i++) {
-        leftH[i] = max(leftH[i - 1], height[i]);
-    }
+        right[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = max(right[i + 1], height[i]);
+        }
 
-    // Build right max array
-    rightH[n - 1] = height[n - 1];
-    for (int i = n - 2; i >= 0; i--) {
-        rightH[i] = max(rightH[i + 1], height[i]);
-    }
+        int ans=0;
+        // skip the first and last idx
+        for(int i=1; i<n-1; i++) {
 
-    // Calculate trapped water
-    for (int i = 0; i < n; i++) {
-        ans += min(leftH[i], rightH[i]) - height[i];
-    }
+            // int leftMax = INT_MIN;
+            // for(int j=i; j>=0; j-- ){
+            //     leftMax = max(leftMax, height[j]);
+            // }
 
-    return ans;
+            // int rightMax = INT_MIN;
+            // for(int j=i; j<n; j++) {
+            //     rightMax = max(rightMax, height[j]);
+            // }
+
+            // int minWater = min(rightMax, leftMax);
+            // ans += minWater-height[i];
+            ans += min(left[i], right[i]) - height[i];
+        }
+        return ans;
+        
     }
 };
