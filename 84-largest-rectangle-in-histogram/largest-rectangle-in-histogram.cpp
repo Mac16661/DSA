16424ca41e88687001b4d657e3f5861e450ba162
Subhodip Ghosh <macy16661@gmail.com>
2025-08-14 18:19:49 +0530

    Time: 18 ms (77.42%) | Memory: 81.4 MB (74.45%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,56 +1,40 @@
 class Solution {
 public:
+    
     int largestRectangleArea(vector<int>& heights) {
-        // int maxr = 0;
-        // for(int i=0; i<heights.size(); i++){
-        //     int area=0;
-        //     int j=i-1;
-        //     // go to left till smaller element is encountered 
-        //     while(j>=0){
-        //         if(heights[i] <= heights[j]){
-        //             area += heights[i];
-        //         }else{
-        //             break;
-        //         }
-        //         j--;
-        //     }
-
-        //     j = i+1;
-        //     // go to right till smaller element is encounterd
-        //     while(j<heights.size()){
-        //         if(heights[i] <= heights[j]){
-        //             area+= heights[i];
-        //         }else{
-        //             break;
-        //         }
-        //         j++;
-        //     }
-
-        //     area += heights[i];
-
-        //     maxr = max(maxr, area);
-        // }
-
-        // return maxr;
-
-
+        int n = heights.size();
         stack<int> st;
         int maxArea = 0;
-        int n = heights.size();
 
-        for (int i = 0; i <= n; ++i) {
-            int currentHeight = (i == n) ? 0 : heights[i];
-
-            while (!st.empty() && heights[st.top()] > currentHeight) {
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n ? 0 : heights[i]);
+            while (!st.empty() && h < heights[st.top()]) {
                 int height = heights[st.top()];
                 st.pop();
                 int width = st.empty() ? i : i - st.top() - 1;
                 maxArea = max(maxArea, height * width);
             }
-
             st.push(i);
         }
-
         return maxArea;
     }
+
+
+    // Brute force:
+    // int largestRectangleArea(vector<int>& heights) {
+    //     int n = heights.size();
+    //     int maxL = 0; // max area
+
+    //     // Check every possible starting point
+    //     for (int i = 0; i < n; i++) {
+    //         int minHeight = heights[i];
+    //         for (int j = i; j < n; j++) {
+    //             minHeight = min(minHeight, heights[j]);
+    //             int width = j - i + 1;
+    //             int area = minHeight * width;
+    //             maxL = max(maxL, area);
+    //         }
+    //     }
+    //     return maxL;   
+    // }
 };
