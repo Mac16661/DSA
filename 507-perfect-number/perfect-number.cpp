8403de8856e72792e68c854c6efb7b86ae77f05f
Subhodip Ghosh <macy16661@gmail.com>
2025-08-16 00:35:16 +0530

    Time: 2113 ms (5.33%) | Memory: 7.7 MB (56.32%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        if( num==6 or num==28 or num==496 or num==8128 or num==33550336) return true;
-
-        return false;
+        // if( num==6 or num==28 or num==496 or num==8128 or num==33550336) return true;
+        int sum=0;
+        for(int i=1; i<num; i++) {
+            if(num%i == 0) {
+                sum+=i;
+                // cout<<i<<" ";
+            }
+        }
+        // cout<<endl<<sum;
+        return sum==num;
     }
 };
