ccfae6c35426142b3c9c9b849a58e4957698002a
Subhodip Ghosh <macy16661@gmail.com>
2025-08-16 14:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (36.06%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int x1 = points[0][0], y1 = points[0][1];
+        int x2 = points[1][0], y2 = points[1][1];
+        int x3 = points[2][0], y3 = points[2][1];
+
+        // Area of triangle method to check collinearity -> SOLELACE FORMULA
+        float area = abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2)) / 2.0;
+
+        return (area == 0) ? false : true;
+    }
+};
