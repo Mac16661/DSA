3c9b20c3656a4f0fb399acb7fdbd05b2c10f48aa
Subhodip Ghosh <macy16661@gmail.com>
2025-08-24 19:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (57.79%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& matrix, vector<bool>& visited) {
+    void dfs(int node, vector<vector<int>>& rooms, vector<bool>& visited) {
         
-        for(int i=0; i<matrix[0].size(); i++) {
-            if(matrix[node][i] == 1 && visited[i] == false) {
-                visited[i] = true;
-                dfs(i, matrix, visited);
+        for(auto room:rooms[node]) {
+            if(visited[room] == false) {
+                visited[room] = true;
+                dfs(room, rooms, visited);
             }
         }
     }
 
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+    bool canVisitAllRooms(vector<vector<int>>& rooms) { // we should have cosidered this rooms as adj list
         int n = rooms.size();
-        vector<vector<int>> matrix(n, vector<int>(n, 0));
-
-        // directed graph
-        for(int i=0; i<rooms.size(); i++) {
-            for(int j = 0; j<rooms[i].size(); j++) {
-                cout<<i<<" "<<j<<endl;
-                int idx = rooms[i][j];
-                matrix[i][idx] = 1; 
-            }
-        }   
-
-        // for(int i=0; i<n; i++) {
-        //     for(int j=0; j<n; j++) {
-        //         cout<<matrix[i][j]<< " ";
-        //     }
-        //     cout<<endl;
-        // }
-
         vector<bool> visited(n, false);
         visited[0] = true;
 
-        dfs(0, matrix, visited);
+        dfs(0, rooms, visited);
 
-        // loop through visited
-        for(auto v: visited) {
-            if(v == false) return false;
+        for( bool v : visited)
+        {
+            if(!v)
+            {
+                return false;
+            }
         }
 
         return true;
