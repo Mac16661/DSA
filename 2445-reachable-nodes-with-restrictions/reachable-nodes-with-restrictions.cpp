5cf9d0263d448d2552340f9910c433b2556a8400
Subhodip Ghosh <macy16661@gmail.com>
2025-09-02 18:20:25 +0530

    Time: 767 ms (5.01%) | Memory: 172.5 MB (55.28%) - LeetSync

---
+++ 2445-reachable-nodes-with-restrictions/reachable-nodes-with-restrictions.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dfs(int node, vector<vector<int>>& graph, vector<int>& visited) {
+        visited[node] = 1;
+        int cnt = 1;
+
+        for(auto adjNode: graph[node]) {
+            if(visited[adjNode] != 1) {
+                cnt += dfs(adjNode, graph, visited);
+            }
+        }
+        cout<<node<<" "<<cnt<<endl;
+        return cnt;
+    }
+
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        vector<vector<int>> graph(n);
+
+        // inserting restricted for constant time access;
+        unordered_set<int> st(restricted.begin(), restricted.end());
+
+        for(auto edge:edges) {
+            if(!st.count(edge[0]) && !st.count(edge[1])){
+                graph[edge[0]].push_back(edge[1]);
+                graph[edge[1]].push_back(edge[0]);
+            }
+        }
+
+        vector<int> visited(n, 0);
+
+        return dfs(0, graph, visited);
+    }
+};
