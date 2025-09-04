19f5f371f13d5645a9f9cf2bb9e926a0b3cb564a
Subhodip Ghosh <macy16661@gmail.com>
2025-09-04 12:31:25 +0530

    Time: 18 ms (59.60%) | Memory: 29.8 MB (23.95%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    // string removeStars(string s) {
+    //     string ans = "";
+
+    //     int i=0;
+    //     while(i<s.size()) {
+    //         if(i!=0 && s[i] == '*') {
+    //             ans = ans.substr(0, ans.size()-1);
+    //         }else{
+    //             if(s[i] != '*') ans.push_back(s[i]);
+    //         }
+    //         // cout<<ans<<endl;
+    //         i++;
+    //     }
+
+    //     return ans;
+    // }
+
+    string removeStars(string s) {
+        stack<char> st;
+
+        int i=0;
+        while(i<s.size()) {
+            if(i!=0 && s[i] == '*') {
+                st.pop();
+            }else{
+                if(s[i] != '*') st.push(s[i]);
+            }
+            // cout<<ans<<endl;
+            i++;
+        }
+
+        string ans;
+
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
