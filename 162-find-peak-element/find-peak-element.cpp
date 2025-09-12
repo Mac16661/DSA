99986c91bba399ae05a89735d582a529340b571b
Subhodip Ghosh <macy16661@gmail.com>
2025-09-12 19:34:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (67.54%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
+    // Optimal  
     int findPeakElement(vector<int>& nums) {
         int n = nums.size()-1;
         if(n==0) return 0;
-        // Edge cases
+
         if(nums[0] > nums[1]) return 0;
         if(nums[n] > nums[n-1]) return n;
 
-        for(int i=1; i<=n-1; i++) {
-            // cout<<nums[i]<<endl;
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+        int low = 1, high = n-1;
+        while(low <= high) {
+            int mid = (low+high)/2;
+
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]) {
+                return mid;
+            }
+
+            // Move towards peak 
+            if(nums[mid-1] < nums[mid]) low = mid+1;
+            else if(nums[mid-1] > nums[mid]) high = mid-1;
+            else low = mid+1; // or high = mid-1; (any one of them could work)
         }
 
-        return 0;
+        return -1;
     }
 };
