3bf1903c2a5a5863772b53bd987a53ff2fac2da1
Subhodip Ghosh <macy16661@gmail.com>
2025-09-14 19:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.10%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    // int fib(int n) {
+        // if(n==0 || n==1) return n;
+
+        // // return fib(n-1)+fib(n-2);
+        // int a = 0;
+        // int b = 1;
+
+        // for(int i=0; i<n; i++) {
+        //     int temp = a+b;
+        //     a = b;
+        //     b = temp;
+        // }
+
+        // return a;
+    // }
+
+    int dp[31];
+
+    Solution() {
+        fill_n(dp, 31, -1);
+    }
+
     int fib(int n) {
-        if(n==0 || n==1) return n;
+        if(n==0) return 0;
+        if(n==1) return 1;
 
-        // return fib(n-1)+fib(n-2);
-        int a = 0;
-        int b = 1;
+        if(dp[n] != -1) return dp[n];
 
-        for(int i=0; i<n; i++) {
-            int temp = a+b;
-            a = b;
-            b = temp;
-        }
-
-        return a;
+        return dp[n] = fib(n-1) + fib(n-2);
     }
 };
