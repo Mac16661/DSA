dba76569d8ff802c59ccd09ada2c75191b704a3d
Subhodip Ghosh <macy16661@gmail.com>
2025-09-20 05:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (57.60%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        int i=0, j=s.size()-1;
+        int n = s.size();
+        int i = 0, j = n-1;
 
         while(i<j) {
             // skiping all non alfa num char
             while( i < j && !isalnum(s[i])) i++;
             while( j > i && !isalnum(s[j])) j--;
-            
+
             if (tolower(s[i]) != tolower(s[j])) return false;
 
-            i++;
-            j--;
-        }
+            i++;j--;
+        } 
 
         return true;
     }
