b854aa62ad4075d8ba0f99d740be07be898b4f9b
Subhodip Ghosh <macy16661@gmail.com>
2025-09-21 08:22:22 +0530

    Time: 21 ms (28.10%) | Memory: 14.7 MB (25.19%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s2, string s1) {
-        if (s1.size() > s2.size()) {};
+    vector<int> findAnagrams(string s, string p) {
+        int i=0;
+        int windowSize = p.size();
+        unordered_map<char, int> smp, pmp;
 
-        unordered_map<char, int> target, window;
-        for (char c : s1) target[c]++;
-
-        int left = 0, right = 0;
-        int k = s1.size();
+        for(auto c: p) {
+            smp[c]++;
+        }
 
         vector<int> ans;
-        while (right < s2.size()) {
-            window[s2[right]]++;
+        for(int j=0; j<s.size(); j++) {
+            pmp[s[j]]++;
 
-            
-            if (right - left + 1 > k) {
-                window[s2[left]]--;
-                if (window[s2[left]] == 0)
-                    window.erase(s2[left]);
-                left++;
+            if(j-i+1 > windowSize) {
+                pmp[s[i]]--;
+                if(pmp[s[i]] == 0) pmp.erase(s[i]);
+                i++;
             }
 
-            
-            if (right - left + 1 == k && window == target) {
-                ans.push_back(left);
+            if(j-i+1 == windowSize && smp == pmp) {
+                ans.push_back(i);
             }
-
-            right++;
         }
 
         return ans;
