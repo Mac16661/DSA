1b7c64cdc12941cccb64ee75d796725d13083d34
Subhodip Ghosh <macy16661@gmail.com>
2025-09-21 06:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (92.25%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -2,23 +2,24 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         vector<int> freq(26, 0);
-    int maxCount = 0;  // frequency of the most frequent character in the window
-    int left = 0;
-    int maxLen = 0;
 
-    for (int right = 0; right < s.size(); right++) {
-        freq[s[right] - 'A']++;
-        maxCount = max(maxCount, freq[s[right] - 'A']);
+        int maxCount = 0; // freq of most frequent char in the window
+        int left = 0;
+        int maxLen = 0;
 
-        // if more than k characters need to be replaced
-        while ((right - left + 1) - maxCount > k) {
-            freq[s[left] - 'A']--;
-            left++;
+        for(int right = 0; right<s.size(); right++) {
+            freq[s[right] - 'A']++;
+            maxCount = max(maxCount, freq[s[right] - 'A']);
+
+            // if more than k character need to be replaced
+            while((right - left + 1) - maxCount > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
         }
 
-        maxLen = max(maxLen, right - left + 1);
-    }
-
-    return maxLen;
+        return maxLen;
     }
 };
