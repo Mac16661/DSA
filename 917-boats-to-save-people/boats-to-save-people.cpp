f0dadfda14eb539027cc867b06af93eaebaba071
Subhodip Ghosh <macy16661@gmail.com>
2025-09-24 04:23:40 +0530

    Time: 11 ms (90.61%) | Memory: 45.7 MB (69.34%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.cpp
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int i =0, j = n-1;
+        int cnt = 0;
         sort(people.begin(), people.end());
 
-        int i =0, j = people.size()-1, boatCnt = 0;
         while(i<=j) {
-            // we are using if instead of while because of people[i] <= limit means limit will not exceed max(people idx 0->n)
-            if(people[i] + people[j] <= limit) i++;
-            j--;
-            boatCnt++;
-        } 
+            // cout<<people[i] <<" "<<people[j]<<endl;
+            if(people[i] + people[j] > limit) {
+                j--;
+                cnt++;
+            }else {
+                i++;
+                j--;
+                cnt++;
+            }
+        }
 
-        return boatCnt;
+        return cnt;
     }
 };
