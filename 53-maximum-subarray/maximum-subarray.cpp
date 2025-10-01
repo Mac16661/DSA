466b91b278c816df3479df8110ff73bd3b8d491d
Subhodip Ghosh <macy16661@gmail.com>
2025-10-01 06:37:03 +0530

    Time: 5 ms (5.26%) | Memory: 82.3 MB (5.02%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
+    int helper(int idx, vector<int>& nums, int n, vector<int>& dp) {
+        if(idx >= n) return 0;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        // take
+        int take = nums[idx] + helper(idx+1, nums, n, dp);
+
+        // not take
+        int notTake = nums[idx];
+
+        return dp[idx] = max(take, notTake);
+    }
+
     int maxSubArray(vector<int>& nums) {
-        int sum = 0, maxSum = INT_MIN;
+        int ans = INT_MIN;
+        int n = nums.size();
+        vector<int> dp(n, -1);
 
-       for(int i=0; i<nums.size(); i++) {
-            sum = sum + nums[i];
-            maxSum = max(maxSum, sum);
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, helper(i, nums, n, dp));
+        }
 
-            if(sum < 0) {
-                sum = 0;
-            } 
-       }
-
-       return maxSum;
+        return ans;
     }
 };
