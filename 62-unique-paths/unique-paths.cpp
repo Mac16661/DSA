ce5e5a69326b2a81c2184537099648cdfb27f7c2
Subhodip Ghosh <macy16661@gmail.com>
2025-10-10 11:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (35.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -19,15 +19,40 @@ public:
     int uniquePaths(int m, int n) {
         // vector<vector<int>> dp(m, vector<int>(n, -1));
         // return findPath(0, 0, m, n, dp);
-        vector<int> prev(n, 1), curr(n, 1);
+        // vector<int> prev(n, 1), curr(n, 1);
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                curr[j] = curr[j - 1] + prev[j];
+        // for (int i = 1; i < m; i++) {
+        //     for (int j = 1; j < n; j++) {
+        //         curr[j] = curr[j - 1] + prev[j];
+        //     }
+        //     prev = curr;
+        // }
+
+        // return prev[n - 1];
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue; // Skip the rest of the loop and continue with the next iteration.
+                } 
+                int up = 0;
+                int left = 0;
+
+                if (i > 0)
+                    up = dp[i - 1][j];
+
+                
+                if (j > 0)
+                    left = dp[i][j - 1];
+
+                // Calculate the number of ways to reach the current cell by adding 'up' and 'left'.
+                dp[i][j] = up + left;
             }
-            prev = curr;
         }
-
-        return prev[n - 1];
+        return dp[m - 1][n - 1];
     }
 };
