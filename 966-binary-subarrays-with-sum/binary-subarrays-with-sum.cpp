0770df9ab19b2350a9e016896b0d70a85731d61f
Subhodip Ghosh <macy16661@gmail.com>
2025-10-19 23:17:12 +0530

    Time: 20 ms (25.15%) | Memory: 38.7 MB (17.47%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> prefixCount;
-        prefixCount[0] = 1;
-        int sum = 0, result = 0;
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        mp[0] = 1;
 
-        for (int num : nums) {
-            sum += num;
-            if (prefixCount.find(sum - goal) != prefixCount.end()) {
-                result += prefixCount[sum - goal];
+        int prefix = 0, cnt = 0;
+
+        for(int i=0; i<n; i++) {
+            prefix += nums[i];
+
+            if(mp.find(prefix - goal) != mp.end()) {
+                cnt += mp[prefix - goal];
             }
-            prefixCount[sum]++;
+
+            mp[prefix]++;
         }
 
-        return result;
+        return cnt;
     }
 };
