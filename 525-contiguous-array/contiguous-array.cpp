00bf850c62b32298a18052f190ef80d7a96fc0e7
Subhodip Ghosh <macy16661@gmail.com>
2025-10-20 00:41:13 +0530

    Time: 22 ms (68.57%) | Memory: 88.4 MB (43.56%) - LeetSync

--- 525-contiguous-array/contiguous-array.cpp
+++ 525-contiguous-array/contiguous-array.cpp
@@ -3,22 +3,17 @@ public:
     int findMaxLength(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int, int> mp;
-        int prefixSum = 0;
-        int maxLen = 0;
 
-        mp[0] = -1; // default case
+        int prefix = 0, maxLen = 0;
+        mp[0] = -1;
 
         for(int i=0; i<n; i++) {
-            prefixSum += (nums[i] == 0 ? -1:1);
-
-            if(mp.count(prefixSum)) {
-                int len = i - mp[prefixSum];
-                maxLen = max(maxLen, len);
-            }else{
-                mp[prefixSum] = i; // only first occurence to get the max len possible 
-            }
+            prefix += (nums[i]==0)?-1:1;
+            if(mp.find(prefix) != mp.end()) {
+                int len = i - mp[prefix];
+                maxLen = max(len , maxLen);
+            }else mp[prefix] = i; // only first occurence to get the lasgest subarr
         }
-
         return maxLen;
     }
 };
