261336187aac03943a639b42555de129bf9344b6
Subhodip Ghosh <macy16661@gmail.com>
2025-10-29 21:41:10 +0530

    Time: 443 ms (35.59%) | Memory: 315.6 MB (43.88%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-    bool dfs(int src, int dest, vector<vector<int>>& graph, vector<bool>& vis) {
-        if(src==dest) return true;
-        bool found = false;
+    void dfs(int node, vector<vector<int>> graph, vector<int>& visited, int dest, bool &reachable) {
+        visited[node] = 1;
 
-        vis[src] = true;
-
-        for(auto it: graph[src]) {
-            if(it == dest) return true;
-            if(!vis[it]) {
-                vis[it] = true;
-                found = found || dfs(it, dest, graph, vis);
-            }
+        if(node == dest) {
+            reachable = true;
         }
-        return found || false;
+
+        for(auto neigh : graph[node]) {
+            if(visited[neigh] == -1) dfs(neigh, graph, visited, dest, reachable);
+        }
     }
 
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        if(source == destination) return true;
-        
-        // Use adjacency list - O(V + E) space and time
+
+        // Creating graph
         vector<vector<int>> graph(n);
-        for(const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+
+        for(auto edge: edges) {
+            int u = edge[0]; 
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
 
-        vector<bool> visited(n, false);
-        visited[source] = true;
-        return dfs(source, destination, graph, visited);
+        vector<int> visited(n, 0);
+        queue<int> q;
+        q.push(source);
+        visited[source] = 1;
 
-        
-        // vector<bool> visited(n, false);
-        // queue<int> q;
-        // visited[source] = true;
-        // q.push(source);
-        
-        // while(!q.empty()) {
-        //     int node = q.front();
-        //     q.pop();
-            
-        //     // Only iterate through actual neighbors (not all n nodes)
-        //     for(int neighbor : graph[node]) {
-        //         if(neighbor == destination) return true;
-                
-        //         if(!visited[neighbor]) {
-        //             visited[neighbor] = true;
-        //             q.push(neighbor);
-        //         }
-        //     }
-        // }
-        // return false;
+        while (!q.empty()) {
+            int node = q.front(); q.pop();
+            if (node == destination) return true;
+            for (int neigh : graph[node]) {
+                if (!visited[neigh]) {
+                    visited[neigh] = 1;
+                    q.push(neigh);
+                }
+            }
+        }
+        return false;
     }
 };
