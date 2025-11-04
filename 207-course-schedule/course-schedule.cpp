e626c359e95c4d014f216b22ce3de2df0ff72c85
Subhodip Ghosh <macy16661@gmail.com>
2025-11-05 00:23:34 +0530

    Time: 8 ms (36.97%) | Memory: 19.4 MB (62.52%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state) {
-        state[node] = 1; // visiting
-
-        for (int neigh : graph[node]) {
-            if (state[neigh] == 1) 
-                return true; // cycle detected
-            if (state[neigh] == 0 && dfs(neigh, graph, state))
-                return true;
-        }
-
-        state[node] = 2; // done visiting
-        return false;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        for (auto& edge : prerequisites) {
-            int a = edge[0];
-            int b = edge[1];
-            graph[b].push_back(a); // correct direction
+        // building grpah
+        int n = numCourses;
+        vector<vector<int>> graph(n);
+        vector<int> indegree(n, 0);
+
+        for(auto &edge: prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+
+            graph[v].push_back(u);
+            indegree[u]++; // indegree 
         }
 
-        vector<int> state(numCourses, 0);
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0) {
-                if (dfs(i, graph, state)) 
-                    return false; // cycle found
+        // topo sort
+        vector<int> topo;
+        queue<int> q;
+        for(int i=0; i<n; i++) {
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(int neigh: graph[node]) {
+                indegree[neigh]--;
+                if(indegree[neigh] == 0) q.push(neigh);
             }
         }
 
-        return true; // no cycle
+        if(n==topo.size()) return true;
+        return false;
     }
 };
