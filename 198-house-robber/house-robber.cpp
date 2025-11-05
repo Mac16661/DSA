01f1ef49f70059a8b3cee0a7b84b8a44aaca0699
Subhodip Ghosh <macy16661@gmail.com>
2025-11-05 22:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (72.80%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -45,21 +45,29 @@ public:
         // return helperDP(n, nums, dp);
 
         // Bottom Up DP
-        
-        int n = nums.size();
-        if(n == 0) return 0;
-        if(n == 1) return nums[0];
+        // int n = nums.size();
+        // if(n == 0) return 0;
+        // if(n == 1) return nums[0];
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // vector<int> dp(n, 0);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
         
-        for(int i=2; i<n; i++) {
-            int nt = dp[i-1];
-            int t = dp[i-2] + nums[i];
-            dp[i] = max(t, nt);
-        } 
+        // for(int i=2; i<n; i++) {
+        //     int nt = dp[i-1];
+        //     int t = dp[i-2] + nums[i];
+        //     dp[i] = max(t, nt);
+        // } 
 
-        return dp[n-1];
+        // return dp[n-1];
+
+        // Space optimization
+        int prev2 = 0, prev1 = 0;
+        for (int num : nums) {
+            int curr = max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
