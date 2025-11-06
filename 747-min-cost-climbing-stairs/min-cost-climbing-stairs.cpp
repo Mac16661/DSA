4c7ca0e21154d5176be5badf95369d7b90631cc6
Subhodip Ghosh <macy16661@gmail.com>
2025-11-06 23:26:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.81%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -40,14 +40,23 @@ public:
         // if(n==2) return min(cost[0], cost[1]);
 
         
-        vector<int> dp(n + 1, 0);
+        // vector<int> dp(n + 1, 0);
 
-        for (int i = 2; i <= n; i++) {
-            int take1 = dp[i - 1] + cost[i - 1];
-            int take2 = dp[i - 2] + cost[i - 2];
-            dp[i] = min(take1, take2);
+        // for (int i = 2; i <= n; i++) {
+        //     int take1 = dp[i - 1] + cost[i - 1];
+        //     int take2 = dp[i - 2] + cost[i - 2];
+        //     dp[i] = min(take1, take2);
+        // }
+
+        // return dp[n];
+
+        int prev2 = 0, prev1 = 0;
+        for (int i = 2; i <= cost.size(); i++) {
+            int curr = min(prev1 + cost[i - 1],
+                        prev2 + cost[i - 2]);
+            prev2 = prev1;
+            prev1 = curr;
         }
-
-        return dp[n];
+        return prev1;
     }
 };
