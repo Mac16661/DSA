7b3cc2f2b927c97576e46cf42f6218702cd45d58
Subhodip Ghosh <macy16661@gmail.com>
2025-11-06 22:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (11.66%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
-    int helper(int idx, vector<int>& cost, vector<int>& dp) {
-        if(idx >= cost.size()) return 0;
-
-        if(dp[idx] != -1) return dp[idx];
+    int n;
+    int minCost = INT_MAX;
+    void helper(int i, vector<int>& cost, int sum) {
+        if(i>=n) {
+            minCost = min(minCost, sum);
+            return;
+        }
 
         // take 1 step
-        int one = cost[idx] + helper(idx+1, cost, dp);
+        helper(i+1, cost, sum+cost[i]);
 
-        // take 2 steps
-        int two = cost[idx] + helper(idx+2, cost, dp);
+        // take 2 step
+        helper(i+2, cost, sum+cost[i]);
+    }
 
-        return dp[idx] = min(one, two);
+    int helperRec(int i, vector<int>& cost, vector<int>& dp) {
+        if(i>=n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int st1 = helperRec(i+1, cost, dp) + cost[i];
+        int st2 = helperRec(i+2, cost, dp) + cost[i];
+
+        return dp[i] =  min(st1, st2);
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int> dp(n+1, 0);
+        n = cost.size();
+        // helper(0, cost, 0); // start from step 0
+        // helper(1, cost, 0); // or step 1
+        // return minCost;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = min(dp[i-1] + cost[i-1], 
-                        dp[i-2] + cost[i-2]);
-        }
-
-        return dp[n];
-        // return min(helper(0, cost, dp), helper(1, cost, dp));
+        vector<int> dp(n, -1);
+        return min(helperRec(0, cost, dp), helperRec(1, cost, dp));
     }
 };
