d9e8b16e3324fe6916d8720645e0510405f4802b
Subhodip Ghosh <macy16661@gmail.com>
2025-11-07 22:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (39.99%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int robLinear(vector<int>& nums, int start, int end) {
-        int prev1 = 0, prev2 = 0;
-        for (int i = start; i <= end; i++) {
-            int pick = nums[i] + prev2;
-            int notPick = prev1;
-            int curr = max(pick, notPick);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+    int helperDP(int n, vector<int> &nums, vector<int> & dp) {
+            // base case
+            if(n < 0) {
+                return 0;
+            }
+
+            // dp
+            if(dp[n] != -1) return dp[n];
+
+            // Take
+            int t = helperDP(n-2, nums, dp) + nums[n];
+
+            // Not Take
+            int nt = helperDP(n-1, nums, dp);
+
+            return dp[n] = max(t, nt);
     }
-    
+
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-        return max(robLinear(nums, 0, n-2), robLinear(nums, 1, n-1));
+        vector<int> nums2(nums.begin()+1, nums.end());
+        vector<int> nums1(nums.begin(), nums.end()-1);
+        vector<int> dp(n - 1, -1);
+        vector<int> dp2(n - 1, -1);
+
+        return max(helperDP(n-2, nums1, dp), helperDP(n-2, nums2, dp2));
     }
 };
