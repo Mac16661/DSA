9941fdc40f99bc463d6b86dca564ffc5c0477aa2
Subhodip Ghosh <macy16661@gmail.com>
2025-11-12 22:55:23 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (46.67%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -11,71 +11,19 @@
  */
 class Solution {
 public:
-    bool checkLeftSubtree(TreeNode* next, TreeNode* root) {
-        if(next==nullptr) return true;
+    bool helper(TreeNode* root, long low, long high) {
+        if(root == nullptr) return true;;
 
-        if(next->val >= root->val) return false;
+        if(root->val <= low || root->val >= high) return false;
 
-        if(!checkLeftSubtree(next->left, root)) return false;
-        if(!checkLeftSubtree(next->right, root)) return false;
+        if(!helper(root->left, low, root->val)) return false;
+        if(!helper(root->right, root->val, high)) return false;
 
         return true;
-    }
-
-    bool checkRightSubtree(TreeNode* next, TreeNode* root) {
-        if(next == nullptr) return true;
-
-        if(next->val <= root->val) return false;
-
-        if(!checkRightSubtree(next->left, root)) return false;
-        if(!checkRightSubtree(next->right, root)) return false;
-        
-        return true;
-    }
-
-    bool valid(TreeNode* node, long left, long right) {
-        if (!node) {
-            return true;
-        }
-        if (!(left < node->val && node->val < right)) {
-            return false;
-        }
-        return valid(node->left, left, node->val) && valid(node->right, node->val, right);
     }
 
     bool isValidBST(TreeNode* root) {
-        if(root == nullptr) return true;
-
-        // Brute force 0(n)^2
-        // TreeNode* temp = root;
-
-        // // Performing dfs
-        // stack<TreeNode*> st;
-        // st.push(root);
-
-        // while(!st.empty()) {
-        //     int size = st.size();
-
-        //     for(int i=0; i<size; i++) {
-        //         TreeNode* f = st.top();
-        //         st.pop();
-
-        //         if(f->right) {
-        //             st.push(f->right);
-        //             if(!checkRightSubtree(f->right, f)) return false;
-        //         }
-
-        //         if(f->left) {
-        //             st.push(f->left);
-        //             if(!checkLeftSubtree(f->left, f)) return false;
-        //         }
-        //     }
-        // }
-
-        // return true;
-
-        // Optimal
-
-        return valid(root, LONG_MIN, LONG_MAX); // root, lower bound, upper bound
+        return helper(root, LONG_MIN, LONG_MAX);
+          
     }
 };
