f8d4159aac296115840ff7741e9adbefe26b7583
Subhodip Ghosh <macy16661@gmail.com>
2025-11-17 21:40:48 +0530

    Time: 28 ms (15.25%) | Memory: 23.6 MB (33.19%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,58 +10,16 @@
 
 class Solution {
 public:
-    void BSTPath(TreeNode* root, TreeNode* t, vector<TreeNode*>& path) {
-        if(root == nullptr) return;
-        path.push_back(root);
-
-        if(root == t) return;
-
-        if(t->val > root->val) BSTPath(root->right, t, path);
-        else BSTPath(root->left, t, path);
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // vector<TreeNode*> pathp;
-        // vector<TreeNode*> pathq;
+        if(root == nullptr) return root;
+        int curr = root->val;
 
-        // BSTPath(root, p, pathp);
-        // BSTPath(root, q, pathq);
-
-        // for(int i=pathp.size()-1; i>=0; i--) {
-        //     for(int j=pathq.size()-1; j>=0; j--) {
-        //         if(pathp[i]->val == pathq[j]->val){
-        //             return pathp[i];
-        //         }
-        //     }
-        // }
-        // return nullptr;
-
-        // Optimal code
-
-        // if(root == nullptr) return root;
-        // int curr = root->val;
-
-        // if(curr < p->val && curr < q->val) {
-        //     return lowestCommonAncestor(root->right, p, q);
-        // }else if(curr > p->val && curr > q->val) {
-        //     return lowestCommonAncestor(root->left, p , q);
-        // }else {
-        //     return root;
-        // }
-
-        // return nullptr;
-
-        // More optimal
-
-        while(root) {
-            int curr = root->val;
-            if(p->val > curr && q->val > curr) {
-                root = root->right;
-            }else if(p->val < curr && q->val < curr) {
-                root = root->left;
-            }else{
-                return root;
-            }
+        if(curr < p->val && curr < q->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        }else if(curr > p->val && curr > q->val) {
+            return lowestCommonAncestor(root->left, p , q);
+        }else {
+            return root;
         }
 
         return nullptr;
