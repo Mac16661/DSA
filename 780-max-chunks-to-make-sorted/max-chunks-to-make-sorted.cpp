5fac249a273c1929888d28f596e3a59cf2e0710c
Subhodip Ghosh <macy16661@gmail.com>
2025-12-08 10:40:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (51.43%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
-    vector<int> NSE(vector<int>& arr) {
-        stack<int> st;
-        vector<int> ans(arr.size(), -1);
-
-        for(int i=arr.size()-1; i>=0; i--) {
-            while(!st.empty() && st.top() >= arr[i]) st.pop();
-
-            if(!st.empty()) ans[i] = st.top();
-
-            st.push(arr[i]);
-        }
-
-        return ans;
-    }
-
     int maxChunksToSorted(vector<int>& arr) {
-    //      stack<int> st;
-    // for (int a : arr) {
-    //     int maxVal = a;
-    //     while (!st.empty() && st.top() > a) {
-    //         maxVal = max(maxVal, st.top());
-    //         st.pop();
-    //     }
-    //     st.push(maxVal);
-    // }
-    // return st.size();
+       // monotonic stack approach
+       int cnt = 0;
+       stack<int> st;
 
-        // Greedily dividing an arr to mantain a property -> only works bec range 0 to n-1 of len n
-        int cnt = 0;
-        int maxVal = 0;
-        for(int i=0; i<arr.size(); i++) {
-            maxVal = max(maxVal , arr[i]);
-            if(maxVal == i) cnt++;
-        }
-
-        return cnt;
-
+       for(int i:arr) {
+            int maxV = i;
+            while(!st.empty() && st.top() > i) {
+                maxV = max(st.top(), maxV);
+                st.pop();
+            }
+            
+            st.push(maxV);
+       } 
+       return st.size();
     }
 };
