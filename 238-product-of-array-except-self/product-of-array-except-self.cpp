f443224c7f9bebfa9049231811c66cbd95dae819
Subhodip Ghosh <macy16661@gmail.com>
2025-12-15 23:41:34 +0530

    Time: 1 ms (50.21%) | Memory: 42.1 MB (31.72%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,19 +4,21 @@ public:
         int n = nums.size();
         vector<int> prefix(n), suffix(n);
 
-        prefix[0] = 1;
-        suffix[n-1] = 1;
+        prefix[0] = nums[0];
+        suffix[n - 1] = nums[n - 1];
 
-        for(int i=1; i<n; i++) prefix[i] = prefix[i-1] * nums[i - 1];
-        for(int i=n-2; i>=0; i--) suffix[i] = suffix[i+1] * nums[i+1];
+        for (int i = 1; i < n; i++)
+            prefix[i] = prefix[i - 1] * nums[i];
+
+        for (int i = n - 2; i >= 0; i--)
+            suffix[i] = suffix[i + 1] * nums[i];
 
         vector<int> ans(n);
 
-        for(int i=0; i<n; i++) {
-            // int left = i > 0 ? prefix[i-1]: prefix[0];
-            // int right = i < n-1 ? suffix[i+1] : suffix[n-1];
-            // cout<<left<<" + "<<right<<endl;
-            ans[i] = prefix[i] * suffix[i];
+        for (int i = 0; i < n; i++) {
+            int left = (i > 0) ? prefix[i - 1] : 1;
+            int right = (i < n - 1) ? suffix[i + 1] : 1;
+            ans[i] = left * right;
         }
 
         return ans;
