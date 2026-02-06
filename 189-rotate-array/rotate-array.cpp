c58a5e2232d63e1f02a12706390a7728c5242d30
Subhodip Ghosh <macy16661@gmail.com>
2026-02-06 23:24:30 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (68.05%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k% nums.size();
+        // Brute O(k * n)
+        // int n = nums.size();
+        // k = k % n; // if it is greater then n it will wrap around
+
+        // for (int i=0; i<k; i++) {
+        //     int last = nums[n-1];
+        //     for (int j = n-1; j > 0; j--) {
+        //         nums[j] = nums[j-1];
+        //     }
+
+        //     nums[0] = last;
+        // }
+
+        // Optimal O(3n)
+        int n = nums.size();
+        k %= n;
 
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin()+k, nums.end());
     }
 };
