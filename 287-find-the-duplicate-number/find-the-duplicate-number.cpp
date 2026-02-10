18422ec2358d463df9ccf26a5ff62d68ab056520
Subhodip Ghosh <macy16661@gmail.com>
2026-02-11 00:08:23 +0530

    Time: 4 ms (60.28%) | Memory: 65.1 MB (68.72%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -4,14 +4,35 @@ public:
         // Brute forece -> use nested loops to determin the duplicates
 
         // Betrer  - > use a set to determin if there is nay duplicate and return
-        int n=nums.size();
-        unordered_set<int> st;
+        // int n=nums.size();
+        // unordered_set<int> st;
 
-        for(int i=0; i<n; i++) {
-            if(st.count(nums[i])) return nums[i];
-            st.insert(nums[i]);
+        // for(int i=0; i<n; i++) {
+        //     if(st.count(nums[i])) return nums[i];
+        //     st.insert(nums[i]);
+        // }
+
+        // return -1;
+
+        // Optimal -> without modifing the array and constant extra space
+        // TODO: Fast and slow pointer apprach
+        // Findig the intersection point
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        // Phase 2: Find entranc to the cycle
+        slow = nums[0];
+
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
 
-        return -1;
+        return slow;
     }
 };
