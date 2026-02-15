80d54e5343d78ae4a7d68ae794b0b50835494fdb
Subhodip Ghosh <macy16661@gmail.com>
2026-02-15 18:03:34 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (76.36%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n = nums.size();
+        // optimal
+        for(auto x:nums) {
+            int idx = abs(x) - 1;  
 
-        for(int i=0; i<n; i++) {
-            int idx = abs(nums[i]) - 1;
-            if(nums[idx] > 0) nums[idx] = -nums[idx];
+            // Marking if not already marked
+            if(nums[idx] > 0) {
+                nums[idx] = -abs(nums[idx]);
+            }
         }
-
+        // Nums[i] is telling for which index it is missing, so naturally we need to do + 1 to get the actual value cosidering everything will be int the sorted order
+        // now just put all the +ve num in ans arr
         vector<int> ans;
-
-        for(int i =0; i<n; i++) {
-            if(nums[i] > 0) ans.push_back(i+1);
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0)
+                ans.push_back(i + 1);   
         }
 
         return ans;
