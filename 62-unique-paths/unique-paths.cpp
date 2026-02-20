a0c1d14fa84f5bf31cca155b605e6f64ccda509b
Subhodip Ghosh <macy16661@gmail.com>
2026-02-21 01:18:49 +0530

    Time: 2 ms (20.04%) | Memory: 9.4 MB (52.34%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,58 +1,20 @@
 class Solution {
 public:
-    int findPath(int row, int col, int m, int n, vector<vector<int>> &dp) {
-        if (row == m - 1 && col == n - 1)
+    int helper(int m, int n, int i, int j, vector<vector<int>>& dp) {
+        if (i == m && j == n) {
             return 1;
+        }
 
-        if (row >= m || col >= n)
-            return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-        if (dp[row][col] != -1)
-            return dp[row][col];
-
-        int down = findPath(row + 1, col, m, n, dp);
-        int right = findPath(row, col + 1, m, n, dp);
-
-        return dp[row][col] = down + right;
+        int way = 0;
+        if (i < m) way += helper(m, n, i + 1, j, dp);
+        if (j < n) way += helper(m, n, i, j + 1, dp);
+        return dp[i][j] = way;
     }
 
-    int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        // return findPath(0, 0, m, n, dp);
-        // vector<int> prev(n, 1), curr(n, 1);
-
-        // for (int i = 1; i < m; i++) {
-        //     for (int j = 1; j < n; j++) {
-        //         curr[j] = curr[j - 1] + prev[j];
-        //     }
-        //     prev = curr;
-        // }
-
-        // return prev[n - 1];
-
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1;
-
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                    continue; // Skip the rest of the loop and continue with the next iteration.
-                } 
-                int up = 0;
-                int left = 0;
-
-                if (i > 0)
-                    up = dp[i - 1][j];
-
-                
-                if (j > 0)
-                    left = dp[i][j - 1];
-
-                // Calculate the number of ways to reach the current cell by adding 'up' and 'left'.
-                dp[i][j] = up + left;
-            }
-        }
-        return dp[m - 1][n - 1];
+    int uniquePaths(int m, int n) {  
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        return helper(m - 1, n - 1, 0, 0, dp);
     }
 };
