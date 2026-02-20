af57bad30d005e67c578a9ac7567dea9a51a6f8f
Subhodip Ghosh <macy16661@gmail.com>
2026-02-21 01:23:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (54.77%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,50 +1,33 @@
 class Solution {
 public:
-    int findPath(int row, int col, int m, int n, vector<vector<int>> &dp, vector<vector<int>>& obstacleGrid) {
-        // Out of bounds or obstacle
-        if (row >= m || col >= n || obstacleGrid[row][col] == 1)
-            return 0;
+    int helper(int m, int n, int i, int j,
+               vector<vector<int>>& grid,
+               vector<vector<int>>& dp) {
 
-        if (row == m - 1 && col == n - 1)
-            return 1;
+        // Out of bounds
+        if (i >= m || j >= n) return 0;
 
-        if (dp[row][col] != -1)
-            return dp[row][col];
+        // Hit obstacle
+        if (grid[i][j] == 1) return 0;
 
-        int down = findPath(row + 1, col, m, n, dp, obstacleGrid);
-        int right = findPath(row, col + 1, m, n, dp, obstacleGrid);
+        // Reached destination
+        if (i == m - 1 && j == n - 1) return 1;
 
-        return dp[row][col] = down + right;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int down = helper(m, n, i + 1, j, grid, dp);
+        int right = helper(m, n, i, j + 1, grid, dp);
+
+        return dp[i][j] = down + right;
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-        // If start or end is blocked, no path exists
-        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1)
-            return 0;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        // return findPath(0, 0, m, n, dp, obstacleGrid);
-        dp[0][0] = 1;
-
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(i==0 && j==0) continue; // base case
-                if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                int up = 0, left = 0;
-
-                if(i>0) up += dp[i-1][j];
-                if(j>0) left += dp[i][j-1];
-
-                dp[i][j] = up+left;
-            }
-        }
-
-        return dp[m-1][n-1];
+        return helper(m, n, 0, 0, obstacleGrid, dp);
     }
 };
