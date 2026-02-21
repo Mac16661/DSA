d5b565e2d0b63b4fd04de15495da07006d637ec0
Subhodip Ghosh <macy16661@gmail.com>
2026-02-21 14:30:14 +0530

    Time: 1 ms (41.55%) | Memory: 16.4 MB (41.48%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j) {
-        int n = grid.size(), m = grid[0].size();
+    // Brute Expore all paths and store the min among them
+    
+    // Better
+    int helper(vector<vector<int>>& grid, vector<vector<int>>& dp, int i, int j, int m, int n)  {
+        // Destination
+        if (i == m-1 && j == n-1) {
+            return grid[i][j];
+        }
 
-        // out of bounds → invalid path
-        if (i >= n || j >= m) return 1e9;
-
-        // destination cell
-        if (i == n-1 && j == m-1) return grid[i][j];
-        
         if(dp[i][j] != -1) return dp[i][j];
 
-        // visit down
-        int down = helper(grid, dp, i+1, j);
+        int down = INT_MAX;
+        int right = INT_MAX;
 
-        // visit right
-        int right = helper(grid, dp, i, j+1);
+        // Move Down
+        if (i + 1 < m) {
+            down = helper(grid, dp, i + 1, j, m, n);
+        }
 
-        return dp[i][j] = grid[i][j] + min(down, right);
+        // Move Right
+        if (j + 1 < n) {
+            right = helper(grid, dp, i, j + 1, m, n);
+        }
+
+        return dp[i][j] = min(down, right) + grid[i][j];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return helper(grid, dp, 0, 0);
+        int m = grid.size();
+        int n = grid[0].size();
+      
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(grid, dp, 0, 0, m, n);
     }
 };
