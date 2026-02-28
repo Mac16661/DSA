16e565ea5328b7676d32ceb657840b80fdc15ba6
Subhodip Ghosh <macy16661@gmail.com>
2026-02-28 19:20:36 +0530

    Time: 393 ms (26.09%) | Memory: 90.1 MB (5.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,43 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int idx,int sum, int target) {
+    // Without memoization 
+    // int helper(vector<int>& nums, int idx,int sum, int target) {
+    //     if(idx >= nums.size()) {
+    //         if(sum == target) return 1;
+    //         return 0;
+    //     }
+
+    //     // add
+    //     int add = helper(nums, idx+1, sum + nums[idx], target);
+
+    //     // Sub
+    //     int sub = helper(nums, idx+1, sum - nums[idx], target);
+
+    //     return add + sub;
+    // }
+    
+    // With memoization 
+    int helper(vector<int>& nums, unordered_map<string,int>& dp,int idx,int sum, int target) {
         if(idx >= nums.size()) {
             if(sum == target) return 1;
             return 0;
         }
 
+        string key = to_string(idx)+ " | " + to_string(sum);
+
+        if(dp.count(key)) return dp[key];
+
         // add
-        int add = helper(nums, idx+1, sum + nums[idx], target);
+        int add = helper(nums, dp, idx+1, sum + nums[idx], target);
 
         // Sub
-        int sub = helper(nums, idx+1, sum - nums[idx], target);
+        int sub = helper(nums, dp, idx+1, sum - nums[idx], target);
 
-        return add + sub;
+        return dp[key] = add + sub;
     }
-    
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return helper(nums, 0, 0, target);
+        unordered_map<string,int> dp;
+        return helper(nums, dp, 0, 0, target);
     }
 };
