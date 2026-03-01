3ec4f3d90addc27110ec711d23d5559f9ef3839a
Subhodip Ghosh <macy16661@gmail.com>
2026-03-01 11:31:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (63.59%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,30 +1,56 @@
 class Solution {
 public:
+    // int findCircleNum(vector<vector<int>>& isConnected) {
+    //     int n = isConnected.size();
+    //     vector<int> visited(n, 0);
+    //     int cnt = 0;
+
+    //     for (int i = 0; i < n; i++) {
+
+    //         if (!visited[i]) {
+    //             cnt++;
+
+    //             queue<int> q;
+    //             q.push(i);
+    //             visited[i] = 1;
+
+    //             while (!q.empty()) {
+    //                 int node = q.front();
+    //                 q.pop();
+
+    //                 for (int j = 0; j < n; j++) {
+    //                     if (isConnected[node][j] == 1 && !visited[j]) {
+    //                         visited[j] = 1;
+    //                         q.push(j);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return cnt;
+    // }
+
+    // DFS Version
+    void dfs(int node, vector<vector<int>>& graph, vector<int>& visited) {
+        visited[node] = 1;
+
+        for (int i = 0; i < graph[node].size(); i++) {
+            if(graph[node][i] == 1 && !visited[i]) 
+                dfs(i, graph, visited);
+        }
+    }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
         vector<int> visited(n, 0);
+
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-
-            if (!visited[i]) {
+        for (int i=0; i<n; i++) {
+            if(!visited[i]) {
                 cnt++;
-
-                queue<int> q;
-                q.push(i);
-                visited[i] = 1;
-
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-
-                    for (int j = 0; j < n; j++) {
-                        if (isConnected[node][j] == 1 && !visited[j]) {
-                            visited[j] = 1;
-                            q.push(j);
-                        }
-                    }
-                }
+                dfs(i, isConnected, visited);
             }
         }
 
