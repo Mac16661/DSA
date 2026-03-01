ed47100890b01d4ca9887c4af41a0a4dd8c40158
Subhodip Ghosh <macy16661@gmail.com>
2026-03-01 11:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (46.04%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
-        vis[node]=1;
-        // push node to ds if need to return the traversal
-
-        for(int i = 0; i < isConnected[0].size(); i++) {
-            if(isConnected[node][i] == 1 && !vis[i]) {
-                dfs(i, isConnected, vis);
-            }
-        }
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> vis(n, 0); // 1 based indexing
-        int cnt =0; // no of connected components
+        vector<int> visited(n, 0);
+        int cnt = 0;
 
-        for(int i=0; i<n; i++) {
-            if(!vis[i]) { // when vis[i] == 0
-                dfs(i, isConnected, vis);
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
                 cnt++;
 
-                // for(auto i:vis) {
-                //     cout<<i<<" ";
-                // }
-                // cout<<" -> "<<i<<endl;
-            } 
+                queue<int> q;
+                q.push(i);
+                visited[i] = 1;
+
+                while (!q.empty()) {
+                    int node = q.front();
+                    q.pop();
+
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[node][j] == 1 && !visited[j]) {
+                            visited[j] = 1;
+                            q.push(j);
+                        }
+                    }
+                }
+            }
         }
 
         return cnt;
