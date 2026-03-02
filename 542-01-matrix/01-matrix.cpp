b6486635ed155ae7b34fb30e5eb5749188d69567
Subhodip Ghosh <macy16661@gmail.com>
2026-03-02 21:09:34 +0530

    Time: 28 ms (24.65%) | Memory: 34.7 MB (83.81%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
+    // Use BFS and we will try from distance 0 th cell to reach 1 if min found then update
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        queue<pair<int,int>> q;
+        vector<vector<int>> dist(n, vector<int>(m, -1));
+        queue<pair<int, int>> q;
 
-        // Step 1: initialize queue with all 0s
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
-                    dist[i][j] = 0;
+        // Put in queue which index are already 0
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(mat[i][j] == 0)
+                   { 
                     q.push({i, j});
-                }
+                    dist[i][j] = 0;
+                    } // setting dist value
             }
         }
 
-        // Directions: up, down, left, right
-        vector<int> dx = {-1, 1, 0, 0};
-        vector<int> dy = {0, 0, -1, 1};
+        vector<pair<int, int>> dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        // Step 2: BFS traversal
-        while (!q.empty()) {
+        while(!q.empty()) {
             auto [r, c] = q.front();
             q.pop();
 
-            for (int k = 0; k < 4; k++) {
-                int nr = r + dx[k];
-                int nc = c + dy[k];
+            for(auto d: dir) {
+                int nr = r + d.first;
+                int nc = c + d.second;
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                    if (dist[nr][nc] > dist[r][c] + 1) {
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && mat[nr][nc] == 1) {
+                    // This is the case when dist is not initialize
+                    if(dist[nr][nc] == -1) {
                         dist[nr][nc] = dist[r][c] + 1;
                         q.push({nr, nc});
                     }
+                    else{
+                        dist[nr][nc] = min(dist[nr][nc], dist[r][c] + 1);
+                    }
+
                 }
             }
         }
-
         return dist;
     }
 };
