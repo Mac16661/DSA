4a163050c512cd8f065cdc18ae8052c03504c5c8
Subhodip Ghosh <macy16661@gmail.com>
2026-03-06 23:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (78.48%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    // This problem is about total how much u can store
     int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
+        if (height.empty()) return 0;
 
-        left[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = max(left[i - 1], height[i]);
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            // Logic: The water level at any point is limited by the shorter side.
+            if (height[left] < height[right]) {
+                // If current height is >= leftMax, we can't trap water, just update max.
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    // Otherwise, we trap the difference.
+                    totalWater += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    totalWater += rightMax - height[right];
+                }
+                right--;
+            }
         }
 
-        right[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = max(right[i + 1], height[i]);
-        }
-
-        int ans=0;
-        // skip the first and last idx
-        for(int i=1; i<n-1; i++) {
-
-            // int leftMax = INT_MIN;
-            // for(int j=i; j>=0; j-- ){
-            //     leftMax = max(leftMax, height[j]);
-            // }
-
-            // int rightMax = INT_MIN;
-            // for(int j=i; j<n; j++) {
-            //     rightMax = max(rightMax, height[j]);
-            // }
-
-            // int minWater = min(rightMax, leftMax);
-            // ans += minWater-height[i];
-            ans += min(left[i], right[i]) - height[i];
-        }
-        return ans;
-        
+        return totalWater;
     }
 };
