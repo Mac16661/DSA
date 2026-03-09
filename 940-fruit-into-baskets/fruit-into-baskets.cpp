0b9243d46343493333103af859afd3340a415c51
Subhodip Ghosh <macy16661@gmail.com>
2026-03-09 20:23:57 +0530

    Time: 54 ms (17.33%) | Memory: 84.7 MB (83.00%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> mpp;
-        
-        int l=0;
-        int r=0;
+        unordered_map<int,int> mp;
+        int n = fruits.size();
+        int j = 0;
+        int cnt = 0;
 
-        int maxi = 0;
+        for (int i=0; i<n; i++) {
+            mp[fruits[i]]++;
 
-        while(r < fruits.size()) {
-            // push fruits[r] into mpp
-            mpp[fruits[r]]++;
+            while (mp.size() > 2) {
+                mp[fruits[j]]--;
+                if (mp[fruits[j]] == 0)
+                    mp.erase(fruits[j]);
 
-            // we need to check how many index in the map if > 2 start increment left ptr and remove mpp[fruits[l]]-- and if mpp[fruits[l]] == 0. run it until mpp.size() == 2
-            while(mpp.size() > 2){
-                mpp[fruits[l]]--;
-                if(mpp[fruits[l]] <= 0) mpp.erase(fruits[l]);   
-                l++;
+                j++;
             }
 
-            // calculate length of window 
-            maxi = max(maxi, r - l + 1);
-
-            // increment r
-            r++;
+            cnt = max(cnt, i - j + 1);
         }
 
-        return maxi;
+        return cnt;
     }
 };
